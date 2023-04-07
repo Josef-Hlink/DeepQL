5ef24492098468f7d0749f03a4be09a9b219508d
Josef <jdhamelink0@gmail.com>
2023-04-07 22:38:46 +0200

    Add random warmup experiment

---
+++ shellscripts/randomwarmup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# exploring what happens when you make the agents take random actions for a while before starting to learn
+# the idea is that this should make the agent avoid the local minima that it gets stuck in initially
+
+DEFAULTS="-nr 5 -ne 10000 -bs 512 -V -g 0.999 -nw 10000"
+NAME="RW"
+
+echo "BASELINE"
+./dqlw.sh $DEFAULTS -I ${NAME}-BL
+
+echo "EXPERIENCE REPLAY"
+./dqlw.sh $DEFAULTS -I ${NAME}-ER -ER -rb 100000
+
+echo "TARGET NETWORK"
+./dqlw.sh $DEFAULTS -I ${NAME}-TN -TN -tf 1000
+
+echo "TARGET NETWORK + EXPERIENCE REPLAY"
+./dqlw.sh $DEFAULTS -I ${NAME}-TR -TN -tf 1000 -ER -rb 100000
